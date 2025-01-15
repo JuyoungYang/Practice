dada708ed62771a4e4dbdcb6bf9da952a1990e7a
JuyoungYang <juyoung.yang11@gmail.com>
2025-01-15 21:07:20 +0900

    Bootstrap 제거

--- 스파르타 과제/3. Django, DRF/user/urls.py
+++ 스파르타 과제/3. Django, DRF/user/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, LoginView  # LoginView 추가
 from . import views
 
 app_name = 'user'
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('login/', LoginView.as_view(
+        template_name='user/login.html',
+        next_page='post:post_list'
+    ), name='login'),
+    path('logout/', LogoutView.as_view(
+        next_page='user:login'
+    ), name='logout'),
     path('profile/', views.profile, name='profile'),
     path('profile/update/', views.profile_update, name='profile_update'),
 ]
--- 스파르타 과제/3. Django, DRF/user/views.py
+++ 스파르타 과제/3. Django, DRF/user/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm, UserUpdateForm
-from .models import CustomUser
 
-# Create your views here.
+
 def signup(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST, request.FILES)
@@ -15,20 +14,6 @@ def signup(request):
     else:
         form = CustomUserCreationForm()
     return render(request, 'user/signup.html', {'form': form})
-
-def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('post:post_list')
-    return render(request, 'user/login.html')
-
-def logout_view(request):
-    logout(request)
-    return redirect('user:login')
 
 @login_required
 def profile(request):
