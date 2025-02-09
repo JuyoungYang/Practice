64424be19d026d79c14e4b02d01a26a4302e8bfa
JuyoungYang <juyoung.yang11@gmail.com>
2025-02-09 20:26:35 +0900

    장고 시크릿키 변경 및 환경변수 설정

--- 스파르타 과제/3. Django, DRF/config/settings.py
+++ 스파르타 과제/3. Django, DRF/config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-2ita&wpu__#@kcm&h4ej(faz-3#oc6%g4y!h14szn!74-8m2-)"
+# .env 파일 로드
+load_dotenv()
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# 환경 변수에서 SECRET_KEY 가져오기
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+# DEBUG 값 가져오기 (없으면 기본값 False)
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+
 
 ALLOWED_HOSTS = []
 
